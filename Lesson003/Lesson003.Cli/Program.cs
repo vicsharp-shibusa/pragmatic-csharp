026bba2154f71b539ba3178182d95f4b7fa27838
Vic Sharp <vicsharp@shibusa.io>
2024-04-27 15:18:34 -0500

    Lesson 3 - Structs.

---
+++ Lesson003/Lesson003.Cli/Program.cs
@@ -0,0 +1,31 @@
+﻿using Structs;
+
+var arr = new ImmutablePoint[10];
+var objs = new object[10];
+
+for (int i = 1, y = 10; i < 11; i++, y--)
+{
+    arr[i-1] = new ImmutablePoint(i / Math.PI, y / Math.PI);
+
+    //objs[i - 1] = arr[i - 1];                 // boxing
+    //arr[i - 1] = (ImmutablePoint)objs[i - 1]; // unboxing
+
+    //var p = new Point(i, y);
+    //var p2 = Point.Copy(p);
+    //Console.WriteLine(p);
+    //Console.WriteLine(p2);
+
+    //if (p == p2)
+    //{
+    //    Console.WriteLine("Equal");
+    //}
+    //else
+    //{
+    //    Console.WriteLine("Not Equal");
+    //}
+}
+
+for (int i = 0; i < arr.Length; i++)
+{
+    Console.WriteLine(arr[i]);
+}
---
+++ Lesson003/Lesson003.Cli/Structs.cs
@@ -0,0 +1,72 @@
+﻿namespace Structs;
+
+public struct Point : IEquatable<Point>
+{
+    public Point(double x, double y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public double X { get; }
+    public double Y { get; }
+
+    public override string ToString() => $"({X},{Y})";
+
+    public Point Copy() => new Point(X, Y);
+
+    public static Point Copy(Point point)  => point.Copy();
+
+    public override bool Equals(object? obj)
+    {
+        return obj is Point point && Equals(point);
+    }
+
+    public bool Equals(Point other)
+    {
+        return X == other.X &&
+               Y == other.Y;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(X, Y);
+    }
+
+    public static bool operator ==(Point left, Point right)
+    {
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(Point left, Point right)
+    {
+        return !(left == right);
+    }
+}
+
+public readonly struct ImmutablePoint
+{
+    public ImmutablePoint(double x, double y)
+    {
+        X = x;
+        Y = y;
+    }
+    public double X { get;  }
+    public double Y { get;  }
+
+    public override string ToString() => $"({X:#,##0.00},{Y:#,##0.00})";
+}
+
+public ref struct RefPoint
+{
+    public RefPoint(double x, double y)
+    {
+        X = x;
+        Y = y;
+    }
+    public double X { get; }
+    public double Y { get; }
+
+    public override string ToString() => $"({X},{Y})";
+}
+
