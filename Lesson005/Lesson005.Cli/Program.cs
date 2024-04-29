ea814cf26353bf8c6c19645e252321e7e74274c3
Vic Sharp <vicsharp@shibusa.io>
2024-04-28 20:26:00 -0500

    Lesson 5 - Records.

---
+++ Lesson005/Lesson005.Cli/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+var p = new Records.Classes.Point(1,2) { Z = 1 };
+var p2 = new Records.Classes.Point(2,1) { Z = 1 };
+
+Console.WriteLine(p);
+Console.WriteLine(p2);
+
+var s = new Records.Structs.Point(1,2);
+var ro = new Records.Structs.ImmutablePoint(2, 1);
+
+Console.WriteLine(s);
+Console.WriteLine(ro);
---
+++ Lesson005/Lesson005.Cli/Records.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+
+namespace Records.Classes
+{
+
+    //public interface IPoint
+    //{
+    //    double X { get; set; }
+    //    double Y { get; set; }
+    //}
+
+    public record class Point(double X, double Y)
+    {
+        public double Z { get; set; }
+
+        protected virtual bool PrintMembers(StringBuilder stringBuilder)
+        {
+            stringBuilder.Append($"({X},{Y})");
+            return true;
+        }
+    }
+
+    //public abstract record class BasePoint
+    //{
+    //    public Guid Id { get; set; } = Guid.NewGuid();
+    //    public virtual double Z { get; set; }
+
+    //}
+}
+
+namespace Records.Structs
+{
+    public record struct Point(double X, double Y) {
+        private bool PrintMembers(StringBuilder stringBuilder)
+        {
+            stringBuilder.Append($"({X},{Y})");
+            return true;
+        }
+    };
+
+    public readonly record struct ImmutablePoint(double X, double Y) {
+        private bool PrintMembers(StringBuilder stringBuilder)
+        {
+            stringBuilder.Append($"({X},{Y})");
+            return true;
+        }
+    };
+}
