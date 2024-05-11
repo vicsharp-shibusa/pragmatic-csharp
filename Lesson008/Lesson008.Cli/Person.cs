76eebb3c41fb7324bc808ddee3c4ccac33ddbaf3
Vic Sharp <vicsharp@shibusa.io>
2024-05-10 19:55:57 -0500

    Lesson 8 - Exceptions and Exception Handling.

---
+++ Lesson008/Lesson008.Cli/Person.cs
@@ -0,0 +1,14 @@
+﻿namespace Lesson008.Cli;
+
+internal class Person
+{
+    public string? Name { get; init; }
+    public int Age { get; init; }
+}
+
+internal class AgeTooOldException : Exception
+{
+    public AgeTooOldException(string? message) : base(message)
+    {
+    }
+}
---
+++ Lesson008/Lesson008.Cli/Program.cs
@@ -0,0 +1,45 @@
+﻿using Lesson008.Cli;
+
+var person = new Person()
+{
+    Name = "Mike",
+    Age = 127
+};
+
+try
+{
+    if (person.Age > 120)
+    {
+        throw new AgeTooOldException("This person is too old");
+    }
+    Console.WriteLine(person.Name.ToCharArray()[0]);
+}
+catch (AgeTooOldException)
+{
+    Console.WriteLine("That person is too old.");
+}
+catch (NullReferenceException exc)
+{
+    Console.WriteLine("null ref");
+    Console.WriteLine(exc);
+}
+catch (Exception exc)
+{
+    Console.WriteLine("other");
+    Console.WriteLine(exc);
+}
+finally
+{
+    Console.WriteLine("This line always happens.");
+}
+
+// Never do this
+
+//try
+//{
+//    // Insert a record into the database.
+//}
+//catch
+//{
+//    // Update the record in the database.
+//}
